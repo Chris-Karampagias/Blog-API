b8cc86f2d09d3255e030f20488e8175963f9d0a5
Chris <karampagiaschris@gmail.com>
2023-10-13 10:56:11 +0300

    Create models

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  authorName: { type: String, maxLength: 30, required: true },
+  title: { type: String, maxLength: 50, required: true },
+  content: { type: String, maxLength: 500, required: true },
+  postedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Comments", commentSchema);
---
+++ models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: { type: String, maxLength: 50, required: true },
+  image: { type: Buffer, contentType: String, required: true },
+  description: { type: String, maxLength: 500, required: true },
+  postedAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+});
+
+module.exports = mongoose.model("Posts", postSchema);
