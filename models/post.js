c036f3f946b64b163049f178cce3b1b9ae2ab2fe
Chris <karampagiaschris@gmail.com>
2023-10-15 20:31:19 +0300

    Edit models config

--- models/post.js
+++ models/post.js
@@ -7,7 +7,9 @@ const postSchema = new Schema({
   description: { type: String, maxLength: 500, required: true },
   postedAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  comments: [
+    { type: Schema.Types.ObjectId, ref: "Comment", default: undefined },
+  ],
 });
 
 module.exports = mongoose.model("Posts", postSchema);
