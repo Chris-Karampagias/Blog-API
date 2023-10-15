634957695de1d229a986792d862167629d6a4e2a
Chris <karampagiaschris@gmail.com>
2023-10-15 20:30:58 +0300

    Add bcrypt hashing

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -1,24 +1,19 @@
 const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
+const bcrypt = require("bcryptjs");
 
 exports.login = asyncHandler(async (req, res, next) => {
   const user = await User.findOne({ username: req.body.username });
-  console.log(user);
   if (!user) {
     return res.status(400).json({ error: "Invalid username" });
   }
-  if (user.password !== req.body.password) {
+  const match = await bcrypt.compare(req.body.password, user.password);
+  if (!match) {
     return res.status(400).json({ error: "Invalid password" });
   }
-  const token = jwt.sign(
-    { id: user._id, username: user.username, password: user.password },
-    process.env.JWT_SECRET_KEY,
-    { expiresIn: "10h" }
-  );
-  res.cookie("token", token, { httpOnly: true, maxAge: 10 * 60 * 60 * 1000 });
-  res.status(200).json({ error: null });
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "10h",
+  });
+  res.status(200).json({ error: null, token });
 });
-
-exports.verify = asyncHandler(async(req, res, (next) => {}));
