ef4c1dd3963ec4ffc2eebb8095d7d70397291b02
Chris <karampagiaschris@gmail.com>
2023-10-17 17:47:19 +0300

    Implement post updating functionality

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,7 @@
-const asyncHandler = require("express-async-handler");
 const Posts = require("../models/post");
 const { body, validationResult } = require("express-validator");
 const multer = require("multer");
+const fs = require("fs");
 
 const storageStrategy = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -31,8 +31,10 @@ exports.newPost = [
       }
       const post = new Posts({
         title: req.body.title.replace("&#x27;", "'"),
-        description: req.body.description,
         image: req.file.path,
+        description: req.body.description,
+        postedAt: new Date(Date.now()),
+        updatedAt: new Date(Date.now()),
         comments: [],
       });
       await post.save();
@@ -56,14 +58,64 @@ exports.viewPosts = async (req, res, next) => {
   }
 };
 
-exports.viewPost = asyncHandler(async (req, res, next) => {
-  res.json({ message: "This is the post route" });
-});
+exports.viewPost = async (req, res, next) => {
+  try {
+    const post = await Posts.findById(req.params.id).exec();
+    if (!post) {
+      res.status(404).json({ error: "Post not found" });
+    }
+    res.json(post);
+  } catch (error) {
+    res.json(error.message);
+  }
+};
 
-exports.updatePost = asyncHandler(async (req, res, next) => {
-  res.json({ message: "This is the update post route" });
-});
+exports.updatePost = [
+  upload.single("image"),
+  body("title", "Title is required").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty() || !req.file) {
+        return res
+          .status(400)
+          .json({ error: [...errors.array(), { msg: "Image is required" }] });
+      }
+      const post = await Posts.findById(req.params.id);
+      const postsUsingOldImage = await Posts.find({
+        image: post.image,
+      })
+        .countDocuments()
+        .exec();
+      if (!postsUsingOldImage > 1) {
+        fs.unlink(`./${post.image}`, (error) => {
+          if (error) {
+            throw error;
+          }
+        });
+      }
 
-exports.deletePost = asyncHandler(async (req, res, next) => {
+      const updatedPost = new Posts({
+        _id: post._id,
+        title: req.body.title.replace("&#x27;", "'"),
+        description: req.body.description,
+        image: req.file.path,
+        postedAt: req.body.postedAt,
+        updatedAt: new Date(Date.now()),
+        comments: req.body.comments,
+      });
+      await Posts.findByIdAndUpdate(post._id, updatedPost);
+      res.status(200).json({ error: null });
+    } catch (error) {
+      res.json({ error });
+    }
+  },
+];
+
+exports.deletePost = async (req, res, next) => {
   res.json({ message: "This is the delete post route" });
-});
+};
--- models/post.js
+++ models/post.js
@@ -1,26 +1,13 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { DateTime } = require("luxon");
 
 const postSchema = new Schema({
   title: { type: String, maxLength: 50, required: true },
   image: { type: String, required: true },
-  description: { type: String, maxLength: 500, required: true },
-  postedAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
+  description: { type: String, required: true },
+  postedAt: { type: Date, required: true },
+  updatedAt: { type: Date, required: true },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comments" }],
-});
-
-postSchema.virtual("postedAtFormatted").get(function () {
-  return DateTime.fromJSDate(this.postedAt).toLocaleString(
-    DateTime.DATETIME_MED
-  );
-});
-
-postSchema.virtual("updatedAtFormatted").get(function () {
-  return DateTime.fromJSDate(this.updatedAt).toLocaleString(
-    DateTime.DATETIME_MED
-  );
 });
 
 module.exports = mongoose.model("Posts", postSchema);
