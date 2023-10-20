1124b11b017ce76f0641e292e1b3d5865ea66eae
Chris <karampagiaschris@gmail.com>
2023-10-20 16:14:48 +0300

    Add separate authenticated routes for cms

--- routes/api.js
+++ routes/api.js
@@ -3,22 +3,35 @@ const userAuth = require("../controllers/userAuth");
 const postsController = require("../controllers/postsController");
 const authentication = require("../middleware/authentication");
 
-router.post("/login", userAuth.login);
-
-router.put("/post/comments", postsController.addComment);
+//Routes for the blog
 
 router.get("/posts/latest", postsController.getLatestPosts);
 
-router.use(authentication);
-
 router.get("/posts", postsController.viewPosts);
 
-router.post("/posts/new", postsController.newPost);
+router.put("/posts/:id/comments", postsController.addComment);
 
 router.get("/posts/:id", postsController.viewPost);
 
-router.put("/posts/:id", postsController.updatePost);
+//Routes for the blog cms
 
-router.delete("/posts/:id", postsController.deletePost);
+router.post("/login", userAuth.login);
+
+router.use(authentication);
+
+router.get("/auth/posts", postsController.viewPosts);
+
+router.get("/auth/posts/:id", postsController.viewPost);
+
+router.post("/auth/posts/new", postsController.newPost);
+
+router.put("/auth/posts/:id", postsController.updatePost);
+
+router.delete("/auth/posts/:id", postsController.deletePost);
+
+router.delete(
+  "/auth/posts/:postId/comments/:commentId",
+  postsController.deleteComment
+);
 
 module.exports = router;
