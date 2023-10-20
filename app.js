f008466ea2009a3e1d4746bc041278b327fd2c30
Chris <karampagiaschris@gmail.com>
2023-10-20 16:58:21 +0300

    Prep for deployment

--- app.js
+++ app.js
@@ -9,12 +9,26 @@ const app = express();
 
 app.use(cors());
 
+//production config
+const compression = require("compression");
+app.use(compression());
+
+const helmet = require("helmet");
+app.use(helmet());
+
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30,
+});
+app.use(limiter);
+
 const apiRouter = require("./routes/api");
 
 //Mongo connection setup
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.MONGO_URL_DEV, {
+  .connect(process.env.MONGO_URL_PROD, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
